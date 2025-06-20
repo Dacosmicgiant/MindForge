307fe3821371a0bba221693ac136c53260d34051
Dacosmicgiant <vedantvanpro@gmail.com>
2025-06-20 23:41:43 +0530

    removing everything

--- mindforge/app/(tabs)/create-habit.jsx
+++
@@ -1,6 +0,0 @@
-// app/(tabs)/create-habit.js - Updated Create Habit Screen
-import CreateHabitForm from '../../components/CreateHabitForm';
-
-export default function CreateHabitScreen() {
-  return <CreateHabitForm />;
-}
